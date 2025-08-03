8842db4a3a672a2d90779fb1f0a22f500723ceca
Montikk <Monty.01@seznam.cz>
2025-08-03 23:13:47 +0200

    hore: add .env support (load with python-dotenv)

--- DjangoProject/settings-legacy.py
+++ DjangoProject/settings-legacy.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vp-^fd)cwbdu(7guj7tg8*rxl9bmc3lg2v1o2xa=@)x78)2gbu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
---
+++ DjangoProject/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+# ALLOWED_HOSTS doplníme před nasazením (např. ["braidella.cz"])
